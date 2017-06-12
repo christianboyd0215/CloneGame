1dec954f7ad742272afb3ffe419824e536ea5c9a
Cai <claudeyxcai@gmail.com>
2017-06-12 01:51:30 -0700

    A package of weapon assets and I import that to the Clone game project(not the master version)

---
+++ Clone/CloneGame/Assets/Weapon/FlyingSpear.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingSpear : MonoBehaviour
+{
+    public Vector2 Speed = new Vector2(15f,0f);
+    private GameObject Character;
+    private Rigidbody2D rb;
+    private bool Shoot = false;
+
+    void Awake()
+    {
+        Character = GameObject.FindWithTag("Player");
+        Debug.Log(Character.transform.localScale.x / Mathf.Abs(Character.transform.localScale.x));
+        Speed = new Vector2(Speed.x * Character.transform.localScale.x / Mathf.Abs(Character.transform.localScale.x), Speed.y*Mathf.Abs(Character.transform.localScale.y));
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        rb.velocity = Speed;
+    }
+    
+    void Update()
+    {
+        Character = GameObject.FindWithTag("Player");
+        Shoot = Input.GetKeyDown(KeyCode.LeftControl);
+        rb.velocity = Speed;
+
+        if (Shoot && Mathf.Abs(Character.transform.position.x - gameObject.transform.position.x) <= 1)
+        {
+            Destroy(gameObject,0.1f);
+        }
+
+        Shoot = false;
+    }
+}
