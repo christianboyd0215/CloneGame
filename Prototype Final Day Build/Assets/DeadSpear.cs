d9aa90789e483f280988a4bb88568acaac784329
东 王 <comicdong@live.cn>
2017-06-13 14:17:38 -0600

    New Folder Added

---
+++ Prototype Final Day Build/Assets/DeadSpear.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadSpear : MonoBehaviour {
+
+    private GameObject Character;
+    private Rigidbody2D rb;
+    private bool Shoot = false;
+
+    void Awake()
+    {
+        Character = GameObject.FindWithTag("Player");
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+
+    void Update()
+    {
+        Character = GameObject.FindWithTag("Player");
+        Shoot = Input.GetKeyDown(KeyCode.LeftControl);
+
+        if (Shoot && Mathf.Abs(Character.transform.position.x - gameObject.transform.position.x) <= 1)
+        {
+            Destroy(gameObject, 0.1f);
+        }
+
+        Shoot = false;
+    }
+}
---
+++ Prototype Final Day Build/Assets/Weapon/HeldSpear.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class HeldSpear : MonoBehaviour {
+    
+    
+    public GameObject FlyingSpear;
+    private GameObject Character;
+    private GameObject Spear;
+    private Rigidbody2D rb;
+    private Vector2 spear_position;
+    private bool Shoot;
+    private bool WasShot = false;
+
+    void Update()
+    {
+        Character = GameObject.FindWithTag("Player");
+        Spear = GameObject.FindWithTag("DeadSpear");
+        Shoot = Input.GetKeyDown(KeyCode.LeftControl);
+        spear_position = gameObject.transform.position;
+        
+        if (Shoot && !WasShot)
+        {
+
+            WasShot = true;
+            gameObject.GetComponent<Renderer>().enabled = false;
+            Instantiate(FlyingSpear, spear_position, Quaternion.identity);
+
+        }
+        else
+        {
+            if (Shoot && WasShot && Mathf.Abs(Spear.transform.position.x - Character.transform.position.x) <= 1) 
+            {
+                WasShot = false;
+                gameObject.GetComponent<Renderer>().enabled = true;
+            }
+        }
+
+        Shoot = false;
+    }
+    
+}
