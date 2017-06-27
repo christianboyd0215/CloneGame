df00c55023783811f9b8817ecfbbdb471e8736a5
christianboyd0215 <christianboyd0215@hotmail.com>
2017-06-27 15:08:43 -0700

    fixed stiched file, spear needs fixed, boss needs scripts, boss room needs work. pause menu please

---
+++ Feature Complete Stich Folder/Assets/FireBall.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+        
+
+	}
+	
+	void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if(trigger.gameObject.CompareTag("FireBallKillzone"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
