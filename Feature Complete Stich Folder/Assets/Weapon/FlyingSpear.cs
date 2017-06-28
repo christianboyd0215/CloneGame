f1f633ea8e35b4bd67bc39108157bdd0338f0568
Cai <claudeyxcai@gmail.com>
2017-06-28 00:46:05 -0600

    Fixed the Spear issues and now the spear can fly in arc. And added limitation of number of times and distance to the function "Press X to call back from far away."

--- Feature Complete Stich Folder/Assets/Weapon/FlyingSpear.cs
+++ Feature Complete Stich Folder/Assets/Weapon/FlyingSpear.cs
@@ -7,45 +7,83 @@ public class FlyingSpear : MonoBehaviour
     public Vector2 Speed = new Vector2(15f, 0f);
     private GameObject Character;
     private Rigidbody2D rb;
-    private GameObject BackBar;
+    public GameObject BackBar;
+    private Vector3 MoveSpeed;
+    private Vector3 GritySpeed = Vector3.zero;
+    private Vector3 currentAngle;
+    public float RecycleRange = 20f;
     private float BackX;
     private float BackY;
+    private float bTime;
+    public float Power = 30;
+    public float Angle = 5;
+    public float Gravity = -10;
+    public float Ground = -19;
+    private float dTime;
+    private float direction;
+    private bool done = false;
     private bool Shoot = false;
     private bool Flying = true;
     public bool Back = false;
-    void Awake()
-    {
-        BackBar = Character = GameObject.FindWithTag("Bar");
-        Character = GameObject.FindWithTag("Player");
-        Speed = new Vector2(Speed.x * Character.transform.localScale.x / Mathf.Abs(Character.transform.localScale.x), Speed.y * Mathf.Abs(Character.transform.localScale.y));
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        rb.velocity = Speed;
-    }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Start()
     {
-        if (!collision.gameObject.CompareTag("Player"))
+        
+        Character = GameObject.FindWithTag("Player");
+        BackBar =  GameObject.FindWithTag("Bar");
+        direction = Character.transform.localScale.x / Mathf.Abs(Character.transform.localScale.x);
+        MoveSpeed = Quaternion.Euler(new Vector3(0, 0, Angle)) * Vector3.right * Power;
+        MoveSpeed = new Vector3(MoveSpeed.x * direction, 1, 1);
+        currentAngle = Vector3.zero;
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Player") && !Back)
         {
             Speed = Vector2.zero;
             Flying = false;
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
         }
+        else
+        {
+            if (collision.gameObject.CompareTag("Player") && Back)
+            {
+                Speed = Vector2.zero;
+                Flying = false;
+                gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            }
+        }
     }
-
-
+    
     void Update()
     {
+        BackBar = GameObject.FindWithTag("Bar");
         Character = GameObject.FindWithTag("Player");
+        
         Shoot = Input.GetKeyDown(KeyCode.LeftControl);
 
-        if (gameObject.transform.position.y <= -19)
+        if (!done)
+        {
+            done = GameObject.Find("Counter").GetComponent<SpearBackCounter>().stage3;
+        }
+
+        if (gameObject.transform.position.y <= Ground)
         {
             gameObject.GetComponent<Rigidbody2D>().simulated = false;
             gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
         }
-        if (BackBar.transform.localScale.x >= 1)
+        if (BackBar != null)
         {
-            _Back();
+            if (BackBar.transform.localScale.x >= 1)
+            {
+                if (!done)
+                {
+                    _Back();
+                }
+            }
         }
 
         if (!Flying)
@@ -55,13 +93,6 @@ public class FlyingSpear : MonoBehaviour
                 Destroy(gameObject, 0.1f);
             }
         }
-        else
-        {
-            if (Shoot && Mathf.Abs(Character.transform.position.x - gameObject.transform.position.x) <= 5)
-            {
-                _Back();
-            }
-        }
 
         if (Back)
         {
@@ -69,31 +100,43 @@ public class FlyingSpear : MonoBehaviour
             if(Mathf.Abs(Character.transform.position.x - gameObject.transform.position.x) <= 1)
             {
                 rb.velocity = Vector2.zero;
+                gameObject.GetComponent<BoxCollider2D>().enabled = true;
                 Flying = false;
             }
         }
-        else
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Flying&&!Back)
         {
-            rb.velocity = Speed;
+            Fly();
         }
 
     }
 
     void _Back()
     {
-        gameObject.GetComponent<Rigidbody2D>().simulated = true;
-        gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-        Flying = true;
-        Back = true;
-        BackX = 15f * (Character.transform.position.x - gameObject.transform.position.x);
-        if (Character.transform.position.y >= gameObject.transform.position.y)
+        if (Mathf.Abs(Character.transform.position.x - gameObject.transform.position.x) <= RecycleRange&& Mathf.Abs(Character.transform.position.y - gameObject.transform.position.y) <= RecycleRange)
         {
-            BackY = Mathf.Abs(Character.transform.position.y - gameObject.transform.position.y);
+                gameObject.GetComponent<Rigidbody2D>().simulated = true;
+                gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+                Flying = true;
+                Back = true;
+                BackX = 15f * (Character.transform.position.x - gameObject.transform.position.x);
+                bTime = Mathf.Abs((Character.transform.position.x - gameObject.transform.position.x)) / 15f;
+                BackY = Mathf.Abs(Character.transform.position.y - gameObject.transform.position.y) / bTime * 2f;
+                Speed = new Vector2(BackX, BackY);
+            
         }
-        else
-        {
-            BackY = -Mathf.Abs(Character.transform.position.y - gameObject.transform.position.y);
-        }
-        Speed = new Vector2(BackX, BackY);
+    }
+
+    void Fly()
+    {
+            GritySpeed.y = Gravity * (dTime += Time.fixedDeltaTime);
+            transform.position += (MoveSpeed + GritySpeed) * Time.fixedDeltaTime;
+            currentAngle.z = Mathf.Atan((MoveSpeed.y + GritySpeed.y) / MoveSpeed.x) * Mathf.Rad2Deg;
+            transform.eulerAngles = currentAngle * 2;
     }
 }
