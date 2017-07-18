61df52b56fd9a3f7109ca28cd3af673522f2f20f
东 王 <comicdong@live.cn>
2017-07-17 18:33:05 -0600

    Start Menu added

---
+++ Feature Complete Stich Folder/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour {
+
+    public string startLevel;
+
+    public void NewGame()
+    {
+        Application.LoadLevel(startLevel);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
