929d015ec434b5a3f1f18297efcb868bb0504c73
christianboyd0215 <christianboyd0215@hotmail.com>
2017-07-27 14:14:17 -0700

    Added new slime boss changes

--- Beta Level/Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
+++ Beta Level/Assets/Standard Assets/CrossPlatformInput/Scripts/Joystick.cs
@@ -25,14 +25,10 @@ namespace UnityStandardAssets.CrossPlatformInput
 		CrossPlatformInputManager.VirtualAxis m_HorizontalVirtualAxis; // Reference to the joystick in the cross platform input
 		CrossPlatformInputManager.VirtualAxis m_VerticalVirtualAxis; // Reference to the joystick in the cross platform input
 
-		void OnEnable()
-		{
-			CreateVirtualAxes();
-		}
-
         void Start()
         {
             m_StartPos = transform.position;
+			CreateVirtualAxes();
         }
 
 		void UpdateVirtualAxes(Vector3 value)
@@ -78,17 +74,17 @@ namespace UnityStandardAssets.CrossPlatformInput
 			if (m_UseX)
 			{
 				int delta = (int)(data.position.x - m_StartPos.x);
-				delta = Mathf.Clamp(delta, - MovementRange, MovementRange);
+				//delta = Mathf.Clamp(delta, - MovementRange, MovementRange);
 				newPos.x = delta;
 			}
 
 			if (m_UseY)
 			{
 				int delta = (int)(data.position.y - m_StartPos.y);
-				delta = Mathf.Clamp(delta, -MovementRange, MovementRange);
+				//delta = Mathf.Clamp(delta, -MovementRange, MovementRange);
 				newPos.y = delta;
 			}
-			transform.position = new Vector3(m_StartPos.x + newPos.x, m_StartPos.y + newPos.y, m_StartPos.z + newPos.z);
+			transform.position = Vector3.ClampMagnitude(new Vector3(newPos.x, newPos.y, newPos.z), MovementRange)+ m_StartPos;
 			UpdateVirtualAxes(transform.position);
 		}
 
--- Beta Level/Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
+++ Beta Level/Assets/Standard Assets/CrossPlatformInput/Scripts/MobileControlRig.cs
@@ -16,13 +16,12 @@ namespace UnityStandardAssets.CrossPlatformInput
         // This define is set or unset by a menu item that is included with
         // the Cross Platform Input package.
 
-
 #if !UNITY_EDITOR
 	void OnEnable()
 	{
 		CheckEnableControlRig();
 	}
-#endif
+	#endif
 
         private void Start()
         {
@@ -38,6 +37,7 @@ namespace UnityStandardAssets.CrossPlatformInput
 
                     o.AddComponent<UnityEngine.EventSystems.EventSystem>();
                     o.AddComponent<UnityEngine.EventSystems.StandaloneInputModule>();
+                    o.AddComponent<UnityEngine.EventSystems.TouchInputModule>();
                 }
             }
         }
@@ -46,15 +46,15 @@ namespace UnityStandardAssets.CrossPlatformInput
 
         private void OnEnable()
         {
-            EditorApplication.update += Update;
             EditorUserBuildSettings.activeBuildTargetChanged += Update;
+            EditorApplication.update += Update;
         }
 
 
         private void OnDisable()
         {
-            EditorApplication.update -= Update;
             EditorUserBuildSettings.activeBuildTargetChanged -= Update;
+            EditorApplication.update -= Update;
         }
 
 
