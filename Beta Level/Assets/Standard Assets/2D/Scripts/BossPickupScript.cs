f61588829f29776f7132bf80af76bfac74f88209
christianboyd0215 <christianboyd0215@hotmail.com>
2017-07-29 13:16:17 -0700

    same as previous commit note

---
+++ Beta Level/Assets/Standard Assets/2D/Scripts/BossPickupScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPickupScript : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject door;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        GameObject newDoor = Instantiate(door, new Vector3(transform.position.x + 1f, transform.position.y, transform.position.z), transform.rotation) as GameObject;
+    }
+
+}
